0b497fed17d9248d1dd99f89526e24ac5898164e
Otto Seiskari <otto.seiskari@gmail.com>
2018-08-21 22:06:22 +0300

    Remove unused dbip

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ docker run -ti --rm -e DISPLAY \
   --cap-add=NET_ADMIN --device /dev/net/tun \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v `pwd`/openvpn:/etc/openvpn \
-  -v `pwd`/dbip:/home/user/dbip \
   -e ASSERT_COUNTRY \
   --dns 1.1.1.1 \
   docker-vpn-browser
