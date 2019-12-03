be00dd0b418ef7b3b4fd3360d4de391024b5857f
karubabu <karubabu@users.noreply.github.com>
2019-12-04 02:23:55 +0900

    run.sh: add --shm-size arg to prevent frequent tab crashes. I don't know if 256M is too big. However, I have confirmed that 128M is not enough( still crashes ).
    reference: https://github.com/pytorch/pytorch/issues/2244

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 set -eux
 docker run -ti --rm -e DISPLAY \
   --cap-add=NET_ADMIN --device /dev/net/tun \
+  --shm-size 256M \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v `pwd`/openvpn:/etc/openvpn \
   -v `pwd`/shared:/home/user/Downloads \
